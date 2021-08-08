67fc244f5bdaa7f1f4af15f255a02646da62b5e4
hamsarajpitani <pitanihamsaraj@gmail.com>
2021-08-08 22:29:22 +0530

    reducer added

---
+++ frontend/redux/store.js
@@ -0,0 +1,7 @@
+import {createStore,applyMiddleware} from "redux";
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from "./root-reducer";
+import thunk from "redux-thunk";
+
+const middleware = [thunk];
+export const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(...middleware)))
